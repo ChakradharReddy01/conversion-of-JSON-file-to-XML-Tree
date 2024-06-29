38d62197944761e6a124829bdc5236f76ab6338e
ChakradharReddy01 <111549918+ChakradharReddy01@users.noreply.github.com>
2024-06-29 07:40:50 +0530

    Update final_code.py

--- final_code.py
+++ final_code.py
@@ -1,12 +1,18 @@
 import json
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET 
 from xml.dom import minidom
-a=input("input json file")
-b=input("input xml file")
-# with open('D:/New_Securin/sampletry2/complete_assign_scratchToEnd/finalcode/finaljson.json', 'r') as f:
+
+
+# conversion of json file to xml tree structure
+
+# a=input("input json file")
+# with open(a, 'r') as f:
 #     data=json.load(f)
-with open(a, 'r') as f:
+
+with open('D:/New_Securin/sampletry2/complete_assign_scratchToEnd/finalcode/finaljson.json', 'r') as f: 
     data=json.load(f)
+
+
 def json_xml(tag,d):
     elem=ET.Element(tag)
     if isinstance(d,dict):
@@ -18,36 +24,31 @@ def json_xml(tag,d):
         for key, val in d.items(): 
             child = json_xml(key, val)
             elem.append(child)
-        # for key,val in d.items():
-        #     child=json_xml(key,val)
-        #     elem.append(child)
     elif isinstance(d,list):
         if tag.lower()!="object":
             elem=ET.Element("array")
             elem.set("name",tag)
         for val in d:
-            if isinstance(val, int):
-                child = ET.Element('number')
-                child.text = str(val)
-            elif isinstance(val,dict):
+            if isinstance(val,dict):
                 child=ET.Element('object')
                 child.text=str(val)
+                child=json_xml("dict",val)
+            elif isinstance(val, bool):
+                child = ET.Element('boolean')
+                child.text = 'true' if val else 'false'
+            elif isinstance(val, int):
+                child = ET.Element('number')
+                child.text = str(val)
             elif isinstance(val, float):
                 child = ET.Element('number')
                 child.text = str(val)
             elif isinstance(val, str):
                 child = ET.Element('string')
                 child.text = val
-            elif isinstance(val, bool):
-                child = ET.Element('boolean')
-                child.text = 'true' if val else 'false'
             else:
                 child = json_xml('array', val)
             elem.append(child)
     elif d is None:
-        # child = ET.Element("null")
-        # child.text=str(d)
-        # elem.append(child)
         elem = ET.Element("null")
         elem.set("name", tag)
        
@@ -56,13 +57,9 @@ def json_xml(tag,d):
             child=ET.Element("boolean")
             child.text=str(d).lower()
             elem.append(child)
-            # child = ET.Element("boolean")
-            # child.text = str(d).lower()
-            # elem.append(child)
         else:
             elem=ET.Element("boolean")
             elem.text=str(d).lower()
-        # elem.append(tag)
             elem.set("name",tag)
         
     elif isinstance(d,(int,float)):
@@ -77,6 +74,8 @@ def json_xml(tag,d):
     else:
         elem.text=str(d)
     return elem
+
+
 root=json_xml('object',data)
 tree=ET.ElementTree(root)
 xml_str=ET.tostring(root)
@@ -84,9 +83,9 @@ xml_str=ET.tostring(root)
 dom = minidom.parseString(xml_str)
 pretty_xml_str = dom.toprettyxml(indent="    ")
 
-# with open('D:/New_Securin/sampletry2/complete_assign_scratchToEnd/finalcode/finalxml.xml','w') as f:
-#     f.write(pretty_xml_str)
-with open(b+'/finalxml.xml','w') as f:
+with open('finalxml.xml','w') as f:
     f.write(pretty_xml_str)
+# with open('outputxml.xml','w') as f:
+#     f.write(pretty_xml_str)
 print(pretty_xml_str)
     
